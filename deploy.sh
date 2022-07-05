b9fb0ce5e2d4b3ab2c868673f5cf317c6e436ae3
dujun <dujun@xraybot.com>
2022-07-05 14:33:53 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/dujungithub/myVitepressMd main:gh-pages
+git push -f https://github.com/dujungithub/viteblog.git main
 
 cd -
